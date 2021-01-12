7263a2c17a2c5d4115542f2b99fe1f277fe9b199
sojungkid@gmail.com <sojungkid@gmail.com>
2021-01-12 19:56:19 +0900

    16.1 commit

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -5,7 +5,7 @@ class Subject extends Component {
     render() { 
       return ( 
         <header>        
-          <h1>{this.props.title}</h1>
+          <h1 href = "/">{this.props.title}</h1>
           {this.props.sub}
         </header>
       );
